d7432923fa0447c15d44c6437cc657c4332be64e
FloydZ <11518727+FloydZ@users.noreply.github.com>
2025-05-14 15:05:44 +0200

    finally building (#3)

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # make sure everything is there
 git submodule update --init
 
+# go into the main directory
+cd src/MeasureSuiteCommandLine
+
 # build assemblyline
 cd deps/AssemblyLine
 ./autogen.sh
@@ -16,16 +19,20 @@ make
 cd ../../
 
 # build local project
-mkdir -p build
-cd build 
-cmake ..
-make 
-cd ..
+#mkdir -p build
+#cd build 
+#cmake ..
+#make 
+#cd ..
 
 # build tests
 cc test/c/test.c -c -o test/test.o
 cc test/c/test2.c -c -o test/test2.o
+cc test/test.o -shared -o test/test.so
 
-cc test/c/test.c -c -o build/test/test.o
-cc test/c/test2.c -c -o build/test/test2.o
-cc build/test/test.o -shared -o build/test/test.so
+#cc test/c/test.c -c -o build/test/test.o
+#cc test/c/test2.c -c -o build/test/test2.o
+#cc build/test/test.o -shared -o build/test/test.so
+
+# going back to the main project directory (not package directory)
+cd ../..
